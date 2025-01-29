f01702274a89cb7a48bb667a5fd6c9a25f7d0464
Rich Bielby <richbielby@e.email>
2025-01-29 10:31:09 +0000

    Added light/full param to get data set versions

--- R/get_dataset_versions.R
+++ R/get_dataset_versions.R
@@ -1,6 +1,8 @@
 #' Get data set versions
 #'
 #' @inheritParams api_url
+#' @param detail Level of detail to return. Given as a character string, it should be one of:
+#' "light" (default) or "full".
 #'
 #' @return Data frame listing all available versions of the given data set
 #' @export
@@ -9,12 +11,21 @@
 #' get_dataset_versions(dataset_id = example_id(group="attendance"))
 get_dataset_versions <- function(
     dataset_id,
+    detail = "light",
     ees_environment = NULL,
     api_version = NULL,
     page_size = 40,
     page = NULL,
     verbose = FALSE) {
+  # Do some basic validation
   validate_page_size(page_size)
+
+  detail <- tolower(detail)
+  if(!(detail %in% c("light", "full"))){
+    stop("The detail parameter should be either \"light\" or \"full\". Value passed: ", detail)
+  }
+
+  # Make the initial API request
   response <- httr::GET(
     api_url(
       dataset_id = dataset_id,
@@ -51,5 +62,25 @@ get_dataset_versions <- function(
     }
   }
   response |> warning_max_pages()
-  return(response$results)
+    results <- response$results |>
+      dplyr::select("version", "type", total_rows = "totalResults") |>
+      cbind(date_released = as.Date(response$results$published))|>
+      cbind(version_title = response$results$release$title)|>
+      cbind(time_period_start = response$results$timePeriod$start)|>
+      cbind(time_period_end = response$results$timePeriod$end)
+  if(detail == "full"){
+      results <- results |>
+        cbind(version_id = response$results$file$id)|>
+        cbind(
+          response$results |>
+            dplyr::select(
+              "status",
+              "notes",
+              geographic_levels = "geographicLevels",
+              "filters",
+              "indicators"
+              )
+          )
+    }
+  return(results)
 }
