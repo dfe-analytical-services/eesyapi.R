0a1b6c807796b115f6c263921f38f3740e6ce3ca
Rich Bielby <richbielby@e.email>
2025-03-04 11:42:33 +0000

    Added some basic tests for parse time codes

--- tests/testthat/test-parse_sqids.R
+++ tests/testthat/test-parse_sqids.R
@@ -12,3 +12,14 @@ test_that("parsing geographic levels", {
     )
   )
 })
+
+test_that("parsing time codes", {
+  expect_error(
+    parse_time_codes(c("NAT", "REG", "LA", "NA"))
+  )
+
+  expect_equal(
+    parse_time_codes(data.frame(code = c("W21", "Academic year"), period = c("2024", "202425"))),
+    data.frame(time_period = c("2024", "202425"), time_identifier = c("Week 21", "Academic year"))
+  )
+})
