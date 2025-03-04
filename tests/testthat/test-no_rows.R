e03f49bb2d43652e469dbde07419627b26e34fcd
Cam Race <52536248+cjrace@users.noreply.github.com>
2025-03-04 17:59:15 +0000

    move test into no rows file

---
+++ tests/testthat/test-no_rows.R
@@ -0,0 +1,19 @@
+test_that("Get publications returns warning when no rows returned", {
+  expect_warning(
+    x <- get_publications(search = "z3x4c5v6b7"),
+    "Your query returned zero rows."
+  )
+})
+
+test_that("Get nice error when no rows are returned", {
+  expect_error(
+    query_dataset(
+      example_id(group = "attendance"),
+      indicators = example_id("indicator", group = "attendance"),
+      time_periods = eesyapi::example_id("time_period", group = "attendance"),
+      geographies = c("SCH"),
+      filter_items = eesyapi::example_id("filter_item", group = "attendance")
+    ),
+    "No rows were returned for your query. Set verbose = TRUE to see detailed API response."
+  )
+})
